f0db3cf6d63d36adddf580fb096828c783ea71af
Itai Meiri <lizardsdance@gmail.com>
2022-11-03 00:33:45 +0200

    added vid bg

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './AboutPage.scss';
 import { motion } from 'framer-motion';
+import VideoBackground from './VideoBackground';
 
 const AboutPage = () => {
   return (
@@ -8,6 +9,7 @@ const AboutPage = () => {
       <motion.div>
         <h1>About Us</h1>
       </motion.div>
+      {/* <VideoBackground /> */}
     </div>
   );
 };
--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import './VideoBackground.scss';
 
 const VideoBackground = () => {
   return (
-    <div>
-        <video loop autoPlay muted id='video-bg'>
+    <>
+    {/* <div className='curtain'></div> */}
+    <div className="video_container">
+        <video width="100%" loop autoPlay muted id='video-bg'>
             <source src={require('../POXT insta teaser 3.mp4')} type='video/mp4' />
         </video>
     </div>
+    </>
   )
 }
 
